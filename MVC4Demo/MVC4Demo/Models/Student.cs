56ada76fd533a192d0e8e1f5e52e2849f6b7b93f
NigelJoCher <chernige@gmail.com>
2016-05-05 15:22:53 -0600

    added more formating attributes and remigrated table

--- MVC4Demo/MVC4Demo/Models/Student.cs
+++ MVC4Demo/MVC4Demo/Models/Student.cs
@@ -9,7 +9,11 @@ namespace MVC4Demo.Models
     public class Student
     {
         public int StudentID { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(50)]
         public string LastName { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
