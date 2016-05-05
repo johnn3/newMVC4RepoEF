dd7fa4b5e1d185d5b69313b337ea17029af53d7e
NigelJoCher <chernige@gmail.com>
2016-05-05 15:53:09 -0600

    Made more changes

---
+++ MVC4Demo/MVC4Demo/Models/Instructor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC4Demo.Models
+{
+    public class Instructor
+    {
+        public int InstructorID { get; set; }
+
+        [Required]
+        [Display(Name = "LastName")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "FirstName")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+        
+        public string FullName
+        {
+            get { return LastName + ", " + FirstName; }
+        }
+
+        public virtual ICollection<Course> Courses { get; set; }
+        public virtual OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
