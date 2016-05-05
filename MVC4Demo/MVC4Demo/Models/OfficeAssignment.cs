76922008d8ad37858dca66baab53f5bbd74a3e59
NigelJoCher <chernige@gmail.com>
2016-05-05 16:11:16 -0600

    added Office Assignment Model

--- MVC4Demo/MVC4Demo/Models/OfficeAssignment.cs
+++ MVC4Demo/MVC4Demo/Models/OfficeAssignment.cs
@@ -1,6 +1,13 @@
-﻿namespace MVC4Demo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC4Demo.Models
 {
     public class OfficeAssignment
     {
+        [Key][ForeignKey("Instructor")]
+        public int InstructorID { get; set; }
+        [StringLength(50)][Display(Name = "Office Location")] 
+        public virtual Instructor Instructor { get; set; }
     }
 }
