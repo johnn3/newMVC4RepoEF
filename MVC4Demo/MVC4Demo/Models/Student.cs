edd88d7973014b998c611fabfbdaf51d816ddb20
NigelJoCher <chernige@gmail.com>
2016-05-05 15:10:46 -0600

    added more data attribute types to the student model with date type

--- MVC4Demo/MVC4Demo/Models/Student.cs
+++ MVC4Demo/MVC4Demo/Models/Student.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 namespace MVC4Demo.Models
 {
     public class Student
     {
-            public int StudentID { get; set; }
-            public string LastName { get; set; }
-            public string FirstMidName { get; set; }
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-mm-dd}", ApplyFormatInEditMode = true)]
             public DateTime EnrollmentDate { get; set; }
             public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
