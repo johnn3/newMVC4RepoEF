679be85d141afe41bb27f3799f95a46649e64a64
NigelJoCher <chernige@gmail.com>
2016-05-05 15:24:29 -0600

    changed more

--- MVC4Demo/MVC4Demo/Models/Student.cs
+++ MVC4Demo/MVC4Demo/Models/Student.cs
@@ -9,10 +9,8 @@ namespace MVC4Demo.Models
     public class Student
     {
         public int StudentID { get; set; }
-        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         [StringLength(50)]
         public string LastName { get; set; }
-        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
