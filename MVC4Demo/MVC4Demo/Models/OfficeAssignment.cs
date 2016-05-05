aa6ace9d714beaeb43b3388c1df7870ec1ab4eab
NigelJoCher <chernige@gmail.com>
2016-05-05 16:14:07 -0600

    commented on the 1-0 relation ship between instructor and OfficeAssignment

--- MVC4Demo/MVC4Demo/Models/OfficeAssignment.cs
+++ MVC4Demo/MVC4Demo/Models/OfficeAssignment.cs
@@ -5,6 +5,8 @@ namespace MVC4Demo.Models
 {
     public class OfficeAssignment
     {
+        //There's a one-to-zero-or-one relationship  between the Instructor and the OfficeAssignment entities
+
         [Key][ForeignKey("Instructor")]
         public int InstructorID { get; set; }
         [StringLength(50)][Display(Name = "Office Location")] 
