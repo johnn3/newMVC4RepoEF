b57c1edf839518da935c42478b4c0e4d017e91e1
Nigel John <nigel.john@wcb.ab.ca>
2016-05-06 11:18:36 -0600

    modified the instructors controller so that it can incorporate the intructor's course taught and id

--- MVC4Demo/MVC4Demo/ViewModels/InstructorIndexData.cs
+++ MVC4Demo/MVC4Demo/ViewModels/InstructorIndexData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC4Demo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,9 @@ namespace MVC4Demo.ViewModels
 {
     public class InstructorIndexData
     {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+
     }
 }
