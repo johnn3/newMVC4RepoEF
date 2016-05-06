189d5bd35fed90d7a32c3815b1024cb44d53cdc2
NigelJoCher <chernige@gmail.com>
2016-05-06 08:38:49 -0600

    modify the course model and add Department model

--- MVC4Demo/MVC4Demo/Models/Course.cs
+++ MVC4Demo/MVC4Demo/Models/Course.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVC4Demo.Models
@@ -7,10 +8,18 @@ namespace MVC4Demo.Models
     {
 
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display( Name = "Name" )]
         public int CourseID { get; set; }
+        [StringLength(50, MinimumLength = 3 )]
         public string Title { get; set; }
+        [Range(0,5)]
         public int Credits { get; set; }
 
+        [Display(Name = "Department")]
+        public int DepartmentID { get; set; }
+
+        public virtual Department Department { get; set; }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
+        public virtual ICollection<Instructor> Instructors { get; set; }
     }
 }
