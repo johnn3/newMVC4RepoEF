bce51d56e84d61078b3c2ec322f946681f5a1fa3
Nigel John <nigel.john@wcb.ab.ca>
2016-05-09 15:22:33 -0600

    done display for the department view, which should work for concurrency (open new tab->save edited record in one and try to save in the other)->the error should then pop up

---
+++ MVC4Demo/MVC4Demo/Controllers/DepartmentsController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC4Demo.DAL;
+using MVC4Demo.Models;
+using System.Data.Entity.Infrastructure;
+
+namespace MVC4Demo.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: Departments
+        public ActionResult Index()
+        {
+            var departments = db.Departments.Include(d => d.Administrator);
+            return View(departments.ToList());
+        }
+
+        // GET: Departments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // GET: Departments/Create
+        public ActionResult Create()
+        {
+            ViewBag.InstructorID = new SelectList(db.Instructors, "InstructorID", "FullName");
+            return View();
+        }
+
+        // POST: Departments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DepartmentID,Name,Budget,StartDate,InstructorID,RowVersion")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departments.Add(department);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstructorID = new SelectList(db.Instructors, "InstructorID", "LastName", department.InstructorID);
+            return View(department);
+        }
+
+        // GET: Departments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.InstructorID = new SelectList(db.Instructors, "InstructorID", "LastName", department.InstructorID);
+            return View(department);
+        }
+
+        // POST: Departments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(
+            [Bind(Include = "DepartmentID,Name,Budget,StartDate,InstructorID,RowVersion")]
+            Department department)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(department).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            catch (DbUpdateConcurrencyException ex)
+            {
+                var entry = ex.Entries.Single();
+                var clientValues = (Department)entry.Entity;
+                var databaseValues = (Department)entry.GetDatabaseValues().ToObject();
+
+                //the code adds a custom error message for each column that
+                //has database values different from what the user entered on the Edit page
+                if (databaseValues.Name != clientValues.Name)
+                    ModelState.AddModelError("Name", "Current value: "
+                        + databaseValues.Name);
+                if (databaseValues.Budget != clientValues.Budget)
+                    ModelState.AddModelError("Budget", "Current value: "
+                        + String.Format("{0:c}", databaseValues.Budget));
+                if (databaseValues.StartDate != clientValues.StartDate)
+                    ModelState.AddModelError("StartDate", "Current value: "
+                        + String.Format("{0:d}", databaseValues.StartDate));
+                if (databaseValues.InstructorID != clientValues.InstructorID)
+                    ModelState.AddModelError("InstructorID", "Current value: "
+                        + db.Instructors.Find(databaseValues.InstructorID).FullName);
+                //A longer error message explains what happened and what to do about it
+                ModelState.AddModelError(string.Empty, "The record you attempted to edit "
+                    + "was modified by another user after you got the original value. The "
+                    + "edit operation was canceled and the current values in the database "
+                    + "have been displayed. If you still want to edit this record, click "
+                    + "the Save button again. Otherwise click the Back to List hyperlink.");
+
+            }
+
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name after DataException and add a line here to write a log.
+                ModelState.AddModelError(string.Empty, "Unable to save changes. Try again, and if the problem persists contact your system administrator.");
+            }
+
+            ViewBag.InstructorID = new SelectList(db.Instructors, "InstructorID", "LastName", department.InstructorID);
+            return View(department);
+        }
+
+        // GET: Departments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Departments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Department department = db.Departments.Find(id);
+            db.Departments.Remove(department);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- MVC4Demo/MVC4Demo/Models/Department.cs
+++ MVC4Demo/MVC4Demo/Models/Department.cs
@@ -19,21 +19,29 @@ namespace MVC4Demo.Models
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
 
+        // A concurrency Property to manage row edits for the department
+        //entity
+
+        //By adding a property you changed the database model, so you need to do another migration.
+        //In the Package Manager Console (PMC), enter the following commands:
+
+        //Add-Migration RowVersion
+        //Update-Database
+
+
+
         //the Column attribute is being used to change SQL data
         //type mapping so that the column will be defined using the SQL Server money type in the database
         [Display(Name = "Administrator")]
-
-        // A conceurrency Property to manage row edits for the department
-        //entity
-        [Timestamp]
-        public byte[] RowVersion { get; set; }
-
-        //•A department may or may not have an administrator, and an administrator 
+         //•A department may or may not have an administrator, and an administrator 
         //is always an instructor. Therefore the InstructorID property is included 
         //as the foreign key to the Instructor entity, and a question mark is added 
         //after the int type designation to mark the property as nullable.The navigation 
         //property is named Administrator but holds an Instructor entity
         public int? InstructorID { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
 
         public virtual Instructor Administrator { get; set; }
         public virtual ICollection<Course> Courses { get; set; }
