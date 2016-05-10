55536f800227d0174e58f1801393a50e5d9764dc
Nigel John <nigel.john@wcb.ab.ca>
2016-05-10 09:34:09 -0600

    minor fixes

--- MVC4Demo/MVC4Demo/Models/Person.cs
+++ MVC4Demo/MVC4Demo/Models/Person.cs
@@ -24,13 +24,13 @@ namespace MVC4Demo.Models
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "first name must be between 2 to 50 characters")]
         [Display(Name = "First Name")]
-        public string FirstName { get; set; }
+        public string FirstMidName { get; set; }
 
         public string FullName
         {
             get
             {
-                return LastName + " " + FirstName;
+                return (LastName + " " + FirstMidName);
             }
         }
 
